c94f1cdccdc8a814cd4461e1c4284cbc93d40b31
Kitaku <86824728+Kitakuxo@users.noreply.github.com>
2021-10-22 21:46:56 +0700

    Update ihereboss_is_monkey.lua

--- ihereboss_is_monkey.lua
+++ ihereboss_is_monkey.lua
@@ -9,6 +9,11 @@ else
 		if game.PlaceId == 2788229376 then
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitakuxo/DAHOODX/main/Dahood.lua", true))()
 		else
+--KL
+		if game.PlaceId == 4520749081 or game.PlaceId == 6381829480 then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitakuxo/KL/main/KL.lua", true))()
+		else
 		    game.Players.LocalPlayer:Kick("Game not found in database.")
 		end
 	end
+end
